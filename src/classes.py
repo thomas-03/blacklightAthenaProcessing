855022a2338523f82d1f64877a0809bc31760451
Tegan Thomas <thomas.astrophysics@gmail.com>
2026-04-16 14:17:51 -0400

    some progress

--- src/classes.py
+++ src/classes.py
@@ -5,12 +5,16 @@ class Image:
         self.filename = filename
         if self.filename[-4:]=='.npz':
             self.file = np.load(self.filename)
-        elif:
-            #put here how we should read in other file formats
-        self.frequencies = self.file['frequency'][:]
-        if(len(self.frequencies)>1):
-            self.mass_msun = self.file['mass_msun'][0]
-            self.width_rg = self.file['width_rg'][0]
         else:
-            self.mass_msun = self.file['mass_msun']
-            self.width_rg = self.file['width_rg']
+            #put here how we should read in other file formats
+            raise NotImplementedError('File format not supported yet. Sorry!')
+        self.frequencies = self.file['frequency'][:]
+        #if(len(self.frequencies)>1):
+        self.mass_msun = self.file['mass_msun'][0]
+        self.width_rg = self.file['width_rg'][0]
+        #else:
+        #    self.mass_msun = self.file['mass_msun']
+        #    self.width_rg = self.file['width_rg']
+        self.adaptive_num_levels = self.file['adaptive_num_levels'][0]
+        self.adaptive_num_blocks = self.file['adaptive_num_blocks'][0]
+        
