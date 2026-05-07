7439c7dcc4d5abaa9bf1a95027d1c04ef9dcbe53
Tegan Thomas <thomas.astrophysics@gmail.com>
2026-05-07 04:08:15 -0400

    added line to stop making empty pngs

--- src/montecarlo/spec2txt.py
+++ src/montecarlo/spec2txt.py
@@ -62,7 +62,7 @@ def plot_one(spectrum, ax, xunit, yunit, imu, iphi, plterr, **kwargs):
             f.write('# '+xlabel+' '+ylabel+' '+ylabel+'_err\n')
             for i in range(len(x)):
                 f.write(f'{myDict["freq_kev"][i]} {myDict["spectrum"][i]} {myDict["spectrum_err"][i]}\n')
-        athenamc.make_plot(x, y, yerr=yerr, xlabel=xlabel, ylabel=ylabel, ax=ax, **kwargs)
+        #athenamc.make_plot(x, y, yerr=yerr, xlabel=xlabel, ylabel=ylabel, ax=ax, **kwargs)
 
 
 
